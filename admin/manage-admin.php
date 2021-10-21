9ddb686175135c0933acf6ccbd040ad4e2b0bf84
ramafaizz12 <ramafais12@gmail.com>
2021-10-21 22:31:06 +0800

    membuat database dan tampilan awal admin

---
+++ admin/manage-admin.php
@@ -0,0 +1,10 @@
+<?php include('partials/menu.php'); ?>      
+      </div>
+      <div class="main-content">
+          <div class="wrapper">
+              <h1>Manage admin</h1>
+            
+          </div>
+          
+      </div>
+      <?php include('partials/footer.php'); ?>
---
+++ admin/partials/footer.php
@@ -0,0 +1,9 @@
+<div class="footer">
+          <div class="wrapper">
+          <p class="text-center">2021 All right reserved, Some restaurant. developed by - <a href="#">Unhas </a></p>
+          </div>
+          
+      </div>
+    </body>
+    
+</html>
